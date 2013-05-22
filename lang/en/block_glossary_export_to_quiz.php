1aca1fea6079e40208ba46519631cbeed74a34aa
Joseph Rézeau <moodle@rezeau.org>
2013-05-22 11:09:17 +0200

    fixed missing refs in en lang file
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- lang/en/block_glossary_export_to_quiz.php
+++ lang/en/block_glossary_export_to_quiz.php
@@ -1,4 +1,28 @@
-<?php 
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'glossary_export_to_quiz', language 'en', branch 'MOODLE_25_STABLE'
+ *
+ * @package    mod
+ * @subpackage quiz
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
 $string['allentries'] = 'All entries';
 $string['glossary_export_to_quiz:addinstance'] = 'Add a new glossary_export_to_quiz block';
 $string['glossary_export_to_quiz:myaddinstance'] = 'Add a new glossary_export_to_quiz block to the My Moodle page';
@@ -48,4 +72,3 @@ $string['shortanswer_1'] = 'Short answer (Case sensitive)';
 $string['sortingorder'] = 'Sorting Order';
 $string['sortingorder_help'] = 'Use this setting to determine how the exported glossary entries will be ordered when you import them to your questions data bank.
 This can be used, in combination with the Maximum number of entries, for creating a quiz to test the latest entries to your glossary (especially a fairly large one). ';
-?>
